1184e4deb9dc59a9717cb34c76fba9c374ca0abe
Riya Mehta <riyamehta1608@gmail.com>
2025-06-28 20:07:42 +0530

    created routes and notes in readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# BACKEND
+
+<!-- 28 June 2025 -->
+
+1. install express
+2. set up express app
+3. set "type":"module" in package.json for import/export
+4. install nodemon as a dev dependency: "npm install nodemon -D" for hot reload
+5. set up scripts:
+   "scripts": {
+   "dev": "node server.js",
+   "start": "nodemon server.js"
+   },
+6. Set up the routes
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,23 @@ import express from "express";
 
 const app = express();
 
-app.get("/api/notes", (req, res) => res.send("You got 5 notes"));
-// http://localhost:5001/api/notes will return us "You got 5 notes" in the browser
+app.get("/api/notes", (req, res) => {
+  // http://localhost:5001/api/notes will return us "You got 5 notes" in the browser
+  res.status(200).send("You got 10 notes");
+});
 
+// to create something, post is used
+app.post("/api/notes", (req, res) =>
+  res
+    .status(201)
+    .json({ message: "A new note using post is created successfully" })
+);
+// to update something, put is used
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note updated using put successfully" });
+});
+//delete note
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note Deleted successfully" });
+});
 app.listen(5001, () => console.log("Server started on port 5001"));
