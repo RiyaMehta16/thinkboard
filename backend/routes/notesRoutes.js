e4b7dc4d4b51b55e82ec51154ad1d02e0e2b9041
Riya Mehta <riyamehta1608@gmail.com>
2025-06-28 23:10:40 +0530

    correction in routes

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -2,17 +2,17 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).send("You got 10 notes");
+  res.status(200).send("You got 40 notes");
 });
 router.post("/", (req, res) => {
   res
     .status(201)
     .json({ message: "A new note using post is created successfully" });
 });
-router.put(":id", (req, res) => {
+router.put("/:id", (req, res) => {
   res.status(200).json({ message: "Note updated using put successfully" });
 });
-router.delete(":id", (req, res) => {
+router.delete("/:id", (req, res) => {
   res.status(200).json({ message: "Note Deleted successfully" });
 });
 
