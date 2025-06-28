d7d7eddcf2a9e8d4e01b7bd51bce279016b2e882
Riya Mehta <riyamehta1608@gmail.com>
2025-06-28 19:32:57 +0530

    my first api creation

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,7 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/notes", (req, res) => res.send("You got 5 notes"));
+// http://localhost:5001/api/notes will return us "You got 5 notes" in the browser
+
 app.listen(5001, () => console.log("Server started on port 5001"));
